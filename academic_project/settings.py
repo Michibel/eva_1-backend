32e10e7b51872ba9204ef39337bcdf2e95007b11
Gabriel <genshincuenta316@gmail.com>
2026-09-09 09:15:41 -0300

    Solucion de error con los templates

--- academic_project/settings.py
+++ academic_project/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # DIRS: Carpeta donde Django buscará plantillas
-        'DIRS': [(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
